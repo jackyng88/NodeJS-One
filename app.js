f54b2d48f9f3ca6cea6a3018061d73384badee32
Jacky <jacky.ng9188@gmail.com>
2019-07-18 14:42:50 -0400

    Routing/Redirecting requests

--- app.js
+++ app.js
@@ -2,25 +2,26 @@
 // it takes either a path to another file, or a core module.
 // examples of core modules - http, https, fs, os, path
 const http = require('http');
+const fs = require('fs');
 
-
-/*
-function requestListener(req, res) {
-
-}
-
-http.createServer(requestListener);
-*/
-
-/*
-http.createServer(function(req,res) {
-
-});
-*/
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
     //process.exit();
+    const url = req.url;
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if (url === '/message' && req.method === 'POST') {
+        fs.writeFileSync('message.txt', 'DUMMY');
+        //res.writeHead(302, {});
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My First Page</title></head>');

cd8a5d0b1d3134b092145a5d1aeb59cbb0416bd2
Jacky <jacky.ng9188@gmail.com>
2019-07-19 11:12:36 -0400

    Parsing bodies of request with chunks/buffer

--- app.js
+++ app.js
@@ -6,32 +6,44 @@ const fs = require('fs');
 
 
 const server = http.createServer((req, res) => {
-    //process.exit();
-    const url = req.url;
-    if (url === '/') {
-        res.write('<html>');
-        res.write('<head><title>Enter Message</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-        res.write('</html>');
-        return res.end();
-    }
-    if (url === '/message' && req.method === 'POST') {
-        fs.writeFileSync('message.txt', 'DUMMY');
-        //res.writeHead(302, {});
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-    }
-    res.setHeader('Content-Type', 'text/html');
+  //process.exit();
+  const url = req.url;
+  if (url === '/') {
     res.write('<html>');
-    res.write('<head><title>My First Page</title></head>');
-    res.write('<body><h1>Hello From my Nodejs Server!</h1></body>');
+    res.write('<head><title>Enter Message</title></head>');
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
     res.write('</html>');
-    res.end();
-    // We should not write to the response any more after end(). Technically you can still call write() but it will
-    // result in an error. You must not change the response after ending it because NodeJS will send the 
-    // response back to the client. Note - the preceding write() statements can be simplified using Express.js
-    
+    return res.end();
+  }
+  if (url === '/message' && req.method === 'POST') {
+    // on() allows us to listen for an event. First arguement is the event, second
+    // is the function that gets invoked from the event.
+    // The data event will fired whenever a new chunk is ready to be read.
+    const body = [];
+    req.on('data', (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.writeFileSync('message.txt', message);
+    });
+    //res.writeHead(302, {});
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    return res.end();
+  }
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My First Page</title></head>');
+  res.write('<body><h1>Hello From my Nodejs Server!</h1></body>');
+  res.write('</html>');
+  res.end();
+  // We should not write to the response any more after end(). Technically you can still call write() but it will
+  // result in an error. You must not change the response after ending it because NodeJS will send the 
+  // response back to the client. Note - the preceding write() statements can be simplified using Express.js
+
 });
 
 // listen starts a process where our node.js will not immediately exit and will "listen".
